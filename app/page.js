94f0bbb2540ae98ed3ccc01078b5c2cfa6a33198
ash2228 <92314878+ash2228@users.noreply.github.com>
2024-07-29 17:49:20 +0530

    weather app

--- app/page.js
+++ app/page.js
@@ -1,113 +1,76 @@
-import Image from "next/image";
+"use client"
+import { useState,useEffect } from "react"
+import {Open_Sans} from "next/font/google"
+const open = Open_Sans({
+  weight:"400",
+  subsets:["cyrillic"]
+})
+export default function Home(){
+  const [info,setInfo] = useState(["loading..."]);
+  const [ip,setIp] = useState(0);
+  const [temp,setTemp] = useState("loading...");
+  const [c,setC] = useState("loading...");
+  const [f,setF] = useState("loading...");
+  const [dis,setDis] = useState(true);
+  useEffect(()=>{
+    fetch("https://api.ipify.org?format=json")
+      .then(res=>res.json())
+      .then(res=>setIp(res.ip)
+    )
+  },[])
+  useEffect(()=>{
+    if(ip){
+      fetch(`http://ip-api.com/json/${ip}`)
+      .then(res=>res.json())
+      .then(res=>setInfo([res.city,res.lat,res.lon]))
+    }
+  },[ip]);
+  useEffect(()=>{
+    if(info[0]!="loading..."){
+      fetch(fetch(`https://api.open-meteo.com/v1/forecast?latitude=${info[1]}&longitude=${info[2]}&current_weather=true`).then(res=>res.json()).then(res=>setTemp(parseInt(res.current_weather.temperature))));
+    }
+  },[info]);
+  useEffect(()=>{
+    if(temp!="loading..."){
+      setC(`${temp}°C`);
+      setF(`${(temp * 9/5) + 32}°F`);
+    }
+  },[temp])
+  return(<>
+  <div className="star top-[10%] left-[20%]"></div>
+    <div className="star top-[30%] left-[70%]"></div>
+    <div className="star top-[50%] left-[40%]"></div>
+    <div className="star top-[70%] left-[80%]"></div>
+    <div className="star top-[90%] left-[10%]"></div>
+    <div className="text-white">
+      <img src="house.png" alt="" className="mix-blend-color-burn xl:h-[40vh] mx-auto"/>
+      <h1 className={`text-center text-3xl ${open.className}`}>Location:{info[0]}</h1>
+      <h1 className={`text-center mt-5 text-3xl ${open.className}`}>Lat:{info[1]} lon:{info[2]}</h1>
+      <div
+  className="flex w-[70%] xl:w-[20%] mx-auto mt-10 space-x-2 border-[3px] border-purple-400 rounded-xl select-none"
+>
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  <label
+    className="radio flex flex-grow items-center justify-center rounded-lg cursor-pointer"
+  >
+    <input type="radio" name="radio" value="react" className="peer hidden" defaultChecked={true} onClick={()=>{setDis(true)}} />
+    <span
+      className="tracking-widest peer-checked:bg-gradient-to-r peer-checked:from-[blueviolet] peer-checked:to-[violet] peer-checked:text-white text-gray-700 p-2 rounded-lg transition duration-150 ease-in-out"
+      >Celcius</span
+    >
+  </label>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  <label
+    className="radio flex flex-grow items-center justify-center rounded-lg p-1 cursor-pointer"
+  >
+    <input type="radio" name="radio" value="vue" className="peer hidden" onClick={()=>{setDis(false)}} />
+    <span
+      className="tracking-widest peer-checked:bg-gradient-to-r peer-checked:from-[blueviolet] peer-checked:to-[violet] peer-checked:text-white text-gray-700 p-2 rounded-lg transition duration-150 ease-in-out"
+      >Ferenhiet</span
+    >
+  </label>
+</div>
+    </div>
+    <h1 className={`text-white ${open.className} text-3xl text-center mt-10`}>{dis?c:f}</h1>
+  </>)
 }
